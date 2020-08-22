a5b4a49a140134303d2cf42a59fd04b59c21e2a1
Hasan Yousef <46838626+hajsf@users.noreply.github.com>
2020-08-22 19:38:25 +0300

    Update Sample.go

--- Sample.go
+++ Sample.go
@@ -1,14 +1,1 @@
-package mjpeg
 
-func ExampleUpdateJPEG() {
-  // Create new buffer for the image
-  buf, _ := gocv.IMEncode(".jpg", img)
-  // Push the new buffer back to the stream
-	stream.UpdateJPEG(buf)
-  // Output:
-  // New stream is pushed to the server
-}
-
-func ExampleUpdateJPEG_options() {
-  // Output:
-}
